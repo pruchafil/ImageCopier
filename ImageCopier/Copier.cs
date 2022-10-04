ec2e8004269b79cad06412258cadf98bfbccd61c
prucha.fi.2019 <prucha.fi.2019@UC313PC17>
2022-10-04 14:40:24 +0200

    Přidejte soubory projektu.

---
+++ ImageCopier/Copier.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageCopier
+{
+    class Copier
+    {
+        private byte[] _bytes;
+        private string _copyPath;
+
+        public bool IsActive { get; set; } = true;
+
+        public Copier(byte[] bytes, string copyPath)
+        {
+            _bytes = bytes;
+            _copyPath = copyPath;
+        }
+
+        public void Proceed()
+        {
+            ulong i = 0;
+            while (IsActive)
+            {
+                File.WriteAllBytes(_copyPath + i++, _bytes);
+            }
+        }
+    }
+}
---
+++ ImageCopier/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ImageCopier
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var grid = new Grid();
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1d, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = GridLength.Auto });
+
+            TextBox tb;
+
+            grid.Children.Add
+            (
+                tb = new TextBox()
+                {
+                    Margin = new Thickness(4d),
+                    Padding = new Thickness(2d),
+                    VerticalAlignment = VerticalAlignment.Center
+                }
+            );
+
+            Button b;
+
+            grid.Children.Add
+            (
+                b = new Button()
+                {
+                    HorizontalAlignment = HorizontalAlignment.Right,
+                    Content = "Zkurvit",
+                    Margin = new Thickness(4d),
+                    Padding = new Thickness(4d, 2d, 4d, 2d)
+                }
+            );
+
+            Grid.SetColumn(tb, 0);
+            Grid.SetColumn(b, 1);
+
+            Copier c = new Copier(null, null);
+
+            b.Click += (s, args) =>
+            {
+                const string start = "Zkurvit",
+                             stop  = "Přestat kurvit";
+
+                if (tb.Text == stop)
+                {
+                    c.IsActive = false;
+                }
+                else
+                {
+                    c = new Copier(File.ReadAllBytes(tb_path.Text), tb_path.Text);
+                }
+
+                b.Content = b.Content.ToString() == start ? stop : start;                
+
+                Task.Run(() =>
+                {
+                    c.Proceed();
+                });
+            };
+
+            stack.Children.Add(grid);
+        }
+    }
+}
